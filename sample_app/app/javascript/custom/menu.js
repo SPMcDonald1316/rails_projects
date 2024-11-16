246a8f14eb1e73d7bc3f19ecd3c76b683704c37e
sean <spmcdonald1316@gmail.com>
2024-11-16 09:46:08 -0600

    make app layout more mobile responsive

--- sample_app/app/javascript/custom/menu.js
+++ sample_app/app/javascript/custom/menu.js
@@ -2,6 +2,13 @@
 
 // Add toggle listeners to listen for clicks
 document.addEventListener('turbo:load', () => {
+  let hamburger = document.querySelector('#hamburger');
+  hamburger.addEventListener('click', (event) => {
+    event.preventDefault();
+    let menu = document.querySelector('#navbar-menu');
+    menu.classList.toggle('collapse');
+  });
+
   let account = document.querySelector('#account');
   account.addEventListener('click', (event) => {
     event.preventDefault();
