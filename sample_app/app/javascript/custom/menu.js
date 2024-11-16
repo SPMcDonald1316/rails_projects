3afc5297119f13904b898ec446b90eec6a1f0c9b
sean <spmcdonald1316@gmail.com>
2024-11-16 09:53:43 -0600

    refactor event listeners to be more dry

--- sample_app/app/javascript/custom/menu.js
+++ sample_app/app/javascript/custom/menu.js
@@ -1,18 +1,17 @@
 // Menu manipulation
 
+// Adds a toggle listener
+const addToggleListener = (selectedId, menuId, toggleClass) => {
+  let element = document.querySelector(`#${selectedId}`);
+  element.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    let menu = document.querySelector(`#${menuId}`);
+    menu.classList.toggle(toggleClass);
+  });
+};
+
 // Add toggle listeners to listen for clicks
 document.addEventListener('turbo:load', () => {
-  let hamburger = document.querySelector('#hamburger');
-  hamburger.addEventListener('click', (event) => {
-    event.preventDefault();
-    let menu = document.querySelector('#navbar-menu');
-    menu.classList.toggle('collapse');
-  });
-
-  let account = document.querySelector('#account');
-  account.addEventListener('click', (event) => {
-    event.preventDefault();
-    let menu = document.querySelector('#dropdown-menu');
-    menu.classList.toggle('active');
-  });
+  addToggleListener('hamburger', 'navbar-menu', 'collapse');
+  addToggleListener('account', 'dropdown-menu', 'active');
 });
